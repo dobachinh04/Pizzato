3ff65913f6ceb69460d8c9e61aa4db064eaf3fc9
geminasnh <tuyenpv2004@gmail.com>
2024-10-04 23:06:19 +0700

    Update create.blade.php

--- resources/views/admin/orders/create.blade.php
+++ resources/views/admin/orders/create.blade.php
@@ -54,7 +54,7 @@
                             
                                     <div class="mb-3">
                                         <label for="user_id" class="form-label">User ID</label>
-                                        <input type="text" class="form-control" id="user_id" name="user_id" required>
+                                       <input type="text" class="form-control" id="user_id" name="user_id" required>
                                     </div>
                             
                                     <div class="mb-3">
