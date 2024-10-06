3962121dd0ecaf4e19bc7bdb175b302f04d5be41
haruuki <oniiisan2k4@gmail.com>
2024-10-06 22:00:40 +0700

    CRUD product

--- app/Models/ProductOption.php
+++ app/Models/ProductOption.php
@@ -5,13 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Category extends Model
+class ProductOption extends Model
 {
     use HasFactory;
-    protected $fillable = [
-        'name',
-        'slug',
-        'status',
-        'show_at_home',
-    ];
 }
--- resources/views/admin/products/edit.blade.php
+++ resources/views/admin/products/edit.blade.php
@@ -11,20 +11,21 @@
 
 <div class="card card-primary">
     <div class="card-header">
-        <h4>Create Procut</h4>
+        <h4>Update Product</h4>
 
     </div>
     <div class="card-body">
-        <form action="{{ route('admin.products.store') }}" method="POST" enctype="multipart/form-data">
+        <form action="{{ route('admin.products.update', $product->id) }}" method="POST" enctype="multipart/form-data">
             @csrf
+            @method('PUT')
             <div class="form-group">
                 <label>Name</label>
-                <input type="text" name="name" class="form-control" value="{{ old('name') }}">
+                <input type="text" name="name" class="form-control" value="{{ old('name', $product->name) }}">
             </div>
 
             <div class="form-group">
                 <label>Slug</label>
-                <input type="text" name="slug" class="form-control" value="{{ old('slug') }}">
+                <input type="text" name="slug" class="form-control" value="{{ old('slug', $product->slug) }}">
             </div>
             <div class="mt-3">
                 <label for="thumb_image" class="form-label">Image</label>
@@ -32,62 +33,65 @@
             </div>
             <div class="form-group">
                 <label>Sku</label>
-                <input type="text" name="sku" class="form-control" value="{{ old('sku') }}">
+                <input type="text" name="sku" class="form-control" value="{{ old('sku', $product->sku) }}">
             </div>
             <div class="form-group">
                 <label for="category_id" class="form-label">Catalogue</label>
                 <select type="text" class="form-select" id="category_id" name="category_id">
                     @foreach ($categories as $id => $name)
-                        <option value="{{ $id }}">{{ $name }}</option>
+                        <option value="{{ $id }}" {{ $product->category_id == $id ? 'selected' : '' }}>
+                            {{ $name }}
+                        </option>
                     @endforeach
                 </select>
             </div>
             <div class="form-group">
                 <label>View</label>
-                <input type="text" name="view" class="form-control" value="{{ old('view') }}">
+                <input type="text" name="view" class="form-control" value="{{ old('view', $product->view) }}">
             </div>
             <div class="form-group">
                 <label>Price</label>
-                <input type="text" name="price" class="form-control" value="{{ old('price') }}">
+                <input type="text" name="price" class="form-control" value="{{ old('price', $product->price) }}">
             </div>
 
             <div class="form-group">
                 <label>Offer Price</label>
-                <input type="text" name="offer_price" class="form-control" value="{{ old('offer_price') }}">
+                <input type="text" name="offer_price" class="form-control"
+                    value="{{ old('offer_price', $product->offer_price) }}">
             </div>
 
             <div class="form-group">
                 <label>Quantity</label>
-                <input type="text" name="qty" class="form-control" value="{{ old('qty') }}">
+                <input type="text" name="qty" class="form-control" value="{{ old('qty', $product->qty) }}">
             </div>
 
             <div class="form-group">
                 <label>Short Description</label>
-                <textarea name="short_description" class="form-control" id="">{{ old('short_description') }}</textarea>
+                <textarea name="short_description" class="form-control" id="">{{ old('short_description', $product->short_description) }}</textarea>
             </div>
 
             <div class="form-group">
                 <label>Long Description</label>
-                <textarea name="long_description" class="form-control summernote" id="">{{ old('long_description') }}</textarea>
+                <textarea name="long_description" class="form-control summernote" id="">{{ old('long_description', $product->long_description) }}</textarea>
             </div>
 
             <div class="form-group">
                 <label>Show at Home</label>
                 <select name="show_at_home" class="form-control" id="">
-                    <option value="1">Yes</option>
-                    <option selected value="0">No</option>
+                    <option value="1" {{ $product->show_at_home == 1 ? 'selected' : '' }}>Yes</option>
+                    <option value="0" {{ $product->show_at_home == 0 ? 'selected' : '' }}>No</option>
                 </select>
             </div>
 
             <div class="form-group">
                 <label>Status</label>
                 <select name="status" class="form-control" id="">
-                    <option value="1">Active</option>
-                    <option value="0">Inactive</option>
+                    <option value="1" {{ $product->status == 1 ? 'selected' : '' }}>Active</option>
+                    <option value="0" {{ $product->status == 0 ? 'selected' : '' }}>Inactive</option>
                 </select>
             </div>
 
-            <button type="submit" class="btn btn-primary">Create</button>
+            <button type="submit" class="btn btn-primary">Edit</button>
         </form>
     </div>
 </div>
