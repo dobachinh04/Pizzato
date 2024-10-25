7e06fd333e2e2e83d2e2370e680065534033478c
dda20102004 <chung2k4ndc@gmail.com>
2024-10-25 21:27:24 +0700

    Update CartController.php

--- app/Http/Controllers/Admin/CartController.php
+++ app/Http/Controllers/Admin/CartController.php
@@ -18,43 +18,31 @@ class CartController extends Controller
         // dd($carts);
     return view('admin.carts.giohang', compact('carts'));
 }
-    
 
-    /**
-     * Show the form for creating a new resource.
-     */
+
     public function create()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
+
     public function store(Request $request)
     {
 
     }
 
-    /**
-     * Display the specified resource.
-     */
+
     public function show(string $id)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
+
     public function edit(string $id)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, string $id)
     {
         $request->validate([
@@ -75,16 +63,16 @@ class CartController extends Controller
     public function destroy(string $id)
     {
         $cart = Cart::findOrFail($id);
-    
-   
+
+
     $cart->delete();
 
     return redirect()->route('admin.carts.giohang')->with('success', 'Sản phẩm đã được xóa khỏi giỏ hàng.');
     }
     public function destroyAll()
 {
-    $userId = auth()->id(); // Lấy ID người dùng hiện tại
-    Cart::where('user_id', $userId)->delete(); // Xóa tất cả giỏ hàng của người dùng
+    $userId = auth()->id();
+    Cart::where('user_id', $userId)->delete();
 
     return redirect()->route('admin.carts.giohang')->with('success', 'Đã xóa tất cả sản phẩm trong giỏ hàng.');
 }
