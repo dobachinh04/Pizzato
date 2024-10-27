f22de1f70f95752d9623814b7b915688666c2de4
dungngduy <148855343+dungngduy@users.noreply.github.com>
2024-10-27 23:09:20 +0700

    client index

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Client\IndexController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,8 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
-Route::get('products', [\App\Http\Controllers\Client\ProductController::class, 'index']);
+// IndexController
+Route::get('/menu', [IndexController::class, 'getMenuPizza']);
+Route::get('/hot-product', [IndexController::class, 'getHotProduct']);
