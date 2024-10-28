7ef90cf51a7c85651429765e545adadaea99bfb2
dda20102004 <chung2k4ndc@gmail.com>
2024-10-28 22:39:21 +0700

    sửa nốt

--- routes/web.php
+++ routes/web.php
@@ -59,7 +59,7 @@ Route::post('/reset-password', [AuthenticationController::class, 'resetPassword'
 
 
 
-Route::get('/admin/carts', [CartController::class, 'index'])->name('admin.carts.giohang');
+Route::get('/client/carts', [CartController::class, 'index'])->name('client.carts.giohang');
 Route::put('/carts/{id}', [CartController::class, 'update'])->name('carts.update');
 Route::delete('/carts/{id}', [CartController::class, 'destroy'])->name('carts.destroy');
 Route::delete('/carts/destroy-all', [CartController::class, 'destroyAll'])->name('carts.destroyAll');
