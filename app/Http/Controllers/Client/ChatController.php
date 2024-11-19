ced6ad67898405e26e7f141821a33e3d8318c006
dda20102004 <chung2k4ndc@gmail.com>
2024-11-19 23:04:58 +0700

    update chat

--- app/Http/Controllers/Client/ChatController.php
+++ app/Http/Controllers/Client/ChatController.php
@@ -9,6 +9,6 @@ class ChatController extends Controller
 {
     public function index()
     {
-        return view('client.chat'); // Adjust the view path if necessary
+        return view('client.chat');
     }
 }
