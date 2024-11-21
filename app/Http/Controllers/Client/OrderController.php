f62bee67b1d36d9ab1e8b2a4eeb9f33a3c96500b
dda20102004 <chung2k4ndc@gmail.com>
2024-11-21 15:05:16 +0700

    update orderController

--- app/Http/Controllers/Client/OrderController.php
+++ app/Http/Controllers/Client/OrderController.php
@@ -5,17 +5,26 @@ namespace App\Http\Controllers\Client;
 use App\Models\Order;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class OrderController extends Controller
 {
-    public function index($userId)
+    public function index()
     {
-        // Lấy tất cả đơn hàng theo user_id và tải các quan hệ liên quan
+        $userId = Auth::id();
         $orders = Order::where('user_id', $userId)
-            ->with(['users','addresses'])
+            ->with(['users', 'addresses'])
             ->get();
 
-            return view('client.order_history.index', compact('orders'));
-
+        return view('client.order_history.index', compact('orders'));
     }
+
+    public function show($orderId)
+    {
+        $order = Order::with(['items.product'])->findOrFail($orderId);
+        $items = $order->items;
+
+        return view('client.order_history.show', compact('order', 'items'));
+    }
+
 }
