ac5c250661193abaf7b2cb650b1d9e97bfb914df
dda20102004 <chung2k4ndc@gmail.com>
2024-12-09 22:31:02 +0700

    Update dashboard.blade.php

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -145,45 +145,7 @@
                                     </div><!-- end card -->
                                 </div><!-- end col -->
 
-                                {{-- <div class="col-xl-3 col-md-6">
-                                    <!-- card -->
-                                    <div class="card card-animate">
-                                        <div class="card-body">
-                                            <div class="d-flex align-items-center">
-                                                <div class="flex-grow-1 overflow-hidden">
-                                                    <p class="text-uppercase fw-medium text-muted text-truncate mb-0">
-                                                        Lượt xem</p>
-                                                </div>
-
-                                            </div>
-                                            <div class="d-flex align-items-end justify-content-between mt-4">
-                                                <div>
-                                                    <h4 class="fs-22 fw-semibold ff-secondary mb-4"><span
-                                                            class="counter-value" data-target="{{ $totalViews }}"></span>
-                                                    </h4>
-                                                    <a href="{{ route('admin.products.index') }}"
-                                                        class="text-decoration-underline">Xem Chi Tiết</a>
-                                                </div>
-
-                                                <style>
-                                                    .view-icon {
-                                                        color: #d53939;
-                                                        /* Màu đỏ */
-                                                        font-size: 24px;
-                                                        /* Kích thước icon */
-                                                    }
-                                                </style>
-                                                <div class="avatar-sm flex-shrink-0">
-                                                    <span class="avatar-title bg-warning-subtle rounded fs-3">
-                                                        <i class="fa-solid fa-eye view-icon"></i>
-                                                        <!-- Icon mắt (lượt xem) -->
-                                                    </span>
-                                                </div>
-                                            </div>
-                                        </div><!-- end card body -->
-                                    </div><!-- end card -->
-                                </div><!-- end col --> --}}
-
+                            
                                 <div class="col-xl-3 col-md-6">
                                     <!-- card -->
                                     <div class="card card-animate">
