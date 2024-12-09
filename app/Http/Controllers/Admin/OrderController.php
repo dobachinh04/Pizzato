dadf7d8067d4a17d133085eb4a91d5965f58d466
Đỗ Bá Chính <dobachinh04@gmail.com>
2024-12-10 00:04:51 +0700

    Sort Latest Orders

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -18,7 +18,7 @@ class OrderController extends Controller
      */
     public function index()
     {
-        $orders = Order::get();
+        $orders = Order::orderBy('created_at', 'desc')->get();
         return view('admin.orders.index', compact('orders'));
     }
 
