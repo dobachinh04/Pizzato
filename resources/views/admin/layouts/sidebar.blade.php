166d0225a429758042dcbc000ebc920d33df1405
Đỗ Bá Chính <dobachinh04@gmail.com>
2024-12-19 00:53:12 +0700

    Hide Slider

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -222,7 +222,7 @@
                     </div>
                 </li>
 
-                <li class="nav-item">
+                {{-- <li class="nav-item">
                     <a class="nav-link menu-link" href="#sidebarSliders" data-bs-toggle="collapse" role="button"
                         aria-expanded="false" aria-controls="sidebarSliders">
                         <i class="fa-solid fa-images"></i> <span data-key="t-landing">Sliders</span>
@@ -240,7 +240,7 @@
                             </li>
                         </ul>
                     </div>
-                </li>
+                </li> --}}
                 <li class="nav-item">
                     <a class="nav-link menu-link" href="#sidebarRefunds" data-bs-toggle="collapse" role="button"
                         aria-expanded="false" aria-controls="sidebarRefunds">
